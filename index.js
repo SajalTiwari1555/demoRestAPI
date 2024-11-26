8513de8c3383536a022e8980d11e1930c677269d
SajalTiwari1555 <sajaltiwari0203908@gmail.com>
2024-11-26 14:49:37 +0530

    done

---
+++ index.js
@@ -0,0 +1,25 @@
+// function mult(a,b){
+//     return a*b;
+// }
+
+// console.log(mult(5,6)); 
+
+// // -D : we use for devdependencies
+
+const express = require('express');
+const app = express();
+
+app.get("/users",(req,res)=>{
+    console.log("the pages is user interaction")
+    res.send({
+        name: 'Abhilash pandey',
+        Marrital_Status: 'married',
+        wife : 'Shreya Chaturvedi',
+        Children:'1 boy and 1 girl',
+        Age: 30,
+    })
+})
+
+app.listen(4000,()=>{
+    console.log('server is running on port 4000')
+})
